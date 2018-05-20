bdea0e4ba50dba452685f9dc8124354816d184de
Pawel Urban <urbanpawel@o2.pl>
2018-05-20 09:43:46 +0200

    getParticipantByLogin Test added

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -47,6 +48,18 @@ public class ParticipantRestControllerTest {
 
 		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
+	}
+	
+	@Test
+	public void getParticipantByLogin() throws Exception{
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(content().string("{\"login\":\"testlogin\",\"password\":\"testpassword\"}"));
+		
 	}
 
 }

1b4fd10d51c35f2c5b4ea635f34c61fc4740cc5b
Pawel Urban <urbanpawel@o2.pl>
2018-05-20 10:17:20 +0200

    delParticipantByLogin Test added

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -4,8 +4,8 @@ import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,7 +23,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ParticipantRestController.class)
@@ -60,7 +59,19 @@ public class ParticipantRestControllerTest {
 		given(participantService.findByLogin("testlogin")).willReturn(participant);
 		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 		.andExpect(jsonPath("login", is("testlogin")));
-		
 	}
+	
+	@Test
+	public void delParticipantByLogin() throws Exception{
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+		
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+		
+		mvc.perform(delete("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent())
+		;
+	}
+
 
 }
